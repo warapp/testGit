31076023408f723602857b6646d0f0e034370fe1
waraphon chamnongphean <waraphon.cha@ku.th>
2022-06-07 20:10:16 +0700

    okey git

---
+++ test1.py
@@ -0,0 +1,17 @@
+# def area(p,e):
+#    c = p*e
+#    return c
+# p = float(input("p :"))
+# e = float(input("e :"))
+# area = (area(p,e))
+# print(area)
+
+
+def area2(w, c):
+    k = 0.5*w*c
+    return k
+
+
+w = float(input("w :"))
+c = float(input("c :"))
+print(area2(w, c))
